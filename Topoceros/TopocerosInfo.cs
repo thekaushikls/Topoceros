e8b23bfab2628c875d344b279803db7098ac5e28
Kaushik <50703773+thekaushikls@users.noreply.github.com>
2023-05-05 18:16:32 +0200

    [added] new project files

---
+++ Topoceros/TopocerosInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace Topoceros
+{
+    public class TopocerosInfo : GH_AssemblyInfo
+    {
+        public override string Name => "Topoceros";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("966c3f2e-5c9c-468f-8ed2-51438acb6f4b");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "Kaushik LS";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
